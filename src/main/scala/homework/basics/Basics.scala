e624087278f3da967a2a92c454b611edf64b4bdf
Ottigan <jmalcans91@gmail.com>
2020-09-28 14:29:40 +0300

    Refactored helper function in Basics by removing the unnecessary parameters

--- src/main/scala/homework/basics/Basics.scala
+++ src/main/scala/homework/basics/Basics.scala
@@ -10,18 +10,18 @@ object Basics extends App {
 
   def gcd(a: Int, b: Int): Int = {
     @tailrec
-    def helper(a: Int, b: Int, i: Int): Int = {
+    def helper(i: Int): Int = {
       if (a % i == 0 && b % i == 0) {
         i
       } else {
-        helper(a, b, i - 1)
+        helper(i - 1)
       }
     }
 
     if (a == 0 && b == 0) 0
     else if (a == 0) Math.abs(b)
     else if (b == 0) Math.abs(a)
-    else if (Math.abs(a) >= Math.abs(b)) helper(a, b, b)
-    else helper(a, b, a)
+    else if (Math.abs(a) >= Math.abs(b)) helper(b)
+    else helper(a)
   }
 }
