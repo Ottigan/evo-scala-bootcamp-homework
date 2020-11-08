0206fd08ff10e3bb1a7c505d726223065dba5287
Ottigan <jmalcans91@gmail.com>
2020-11-08 08:01:40 +0200

    Initial commit for Effects HW

---
+++ src/test/scala/homework/effects/EffectsHomework1Spec.scala
@@ -0,0 +1,11 @@
+package homework.effects
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class EffectsHomework1Spec extends AnyFunSuite {
+
+  test("invoking pure() should return a Unit") {
+    assert(???)
+  }
+
+}
