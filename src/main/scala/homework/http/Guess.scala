392de19e903c0bbdba551ff6932656fe14b48150
Ottigan <jmalcans91@gmail.com>
2020-11-17 17:00:33 +0200

    Progress with HTTP HW.... not really

--- src/main/scala/homework/http/Guess.scala
+++ src/main/scala/homework/http/Guess.scala
@@ -8,6 +8,7 @@ import org.http4s.implicits._
 import org.http4s.server.blaze.BlazeServerBuilder
 import org.http4s.client.blaze.BlazeClientBuilder
 import org.http4s.circe.CirceEntityCodec._
+import org.http4s.Status.{NotFound, Successful}
 
 import scala.concurrent.ExecutionContext
 import io.circe.generic.auto._
@@ -30,7 +31,23 @@ import scala.util.Random
 //
 // Use HTTP or WebSocket for communication. The exact protocol and message format to use is not specified and
 // should be designed while working on the task.
-final case class Game(min: Int, max: Int, attempts: Int)
+final case class Template(min: Int, max: Int, attempts: Int)
+final case class Game(min: Int, max: Int, attempts: Int, numberToGuess: Int, id: String)
+object Game {
+  def create(t: Template, number: Int, id: String): Game = Game(t.min, t.max, t.attempts, number, id)
+}
+final case class CurrentGames(games: List[Game]) {
+  def addGame(game: Game): CurrentGames = CurrentGames.create(games :+ game)
+  def removeGame(game: Game): CurrentGames = CurrentGames.create(games.filterNot(_ == game))
+  def updateGame(game: Game): CurrentGames = {
+    val updatedGame = game.copy(attempts = game.attempts - 1)
+
+    removeGame(game).addGame(updatedGame)
+  }
+}
+object CurrentGames {
+  def create(games: List[Game]): CurrentGames = CurrentGames(games)
+}
 
 object GuessServer extends IOApp {
 
@@ -43,52 +60,57 @@ object GuessServer extends IOApp {
       .drain
       .as(ExitCode.Success)
 
+  var currentGames: CurrentGames = CurrentGames.create(Nil)
+
   private val httpApp = HttpRoutes.of[IO] {
 
     // curl -POST -v "localhost:3001/game" -d '{"min": 0, "max": 5, "attempts": 3}' -H "Content-Type: application/json"
     case req @ POST -> Root / "game"                          =>
-      req.as[Game]
-        .flatMap(game => {
-          val randomNumber = Random.between(game.min, game.max)
-          Ok(s"Game has started, you may begin guessing between ${game.min} and ${game.max}")
-            .map(_
-              .addCookie("attemptsLeft", game.attempts.toString)
-              .addCookie("numberToGuess", randomNumber.toString))
+      req.as[Template]
+        .flatMap(template => {
+          val randomNumber = Random.between(template.min, template.max + 1)
+          val gameID = java.util.UUID.randomUUID.toString
+          val formattedGame = Game.create(template, randomNumber, gameID)
+          currentGames = currentGames.addGame(formattedGame)
+
+          Ok(s"Game has started, you may begin guessing between ${template.min} and ${template.max}")
+            .map(_.addCookie("gameID", gameID))
         })
 
-    // curl -v "localhost:3001/game/guess/2" -b "numberToGuess=3;attemptsLeft=3"
+    // curl -v "localhost:3001/game/guess/2" -b "gameID=648afa12-94c1-43cf-97e0-243708ba77f0"
     case req @ GET -> Root / "game" / "guess" / IntVar(guess) =>
-      val numberToGuessCookie = req.cookies.find(_.name == "numberToGuess")
-      val numberToGuessValue = numberToGuessCookie.flatMap(_.content.toIntOption)
-      val attemptsLeftCookie = req.cookies.find(_.name == "attemptsLeft")
-      val attemptsLeftValue = attemptsLeftCookie.flatMap(_.content.toIntOption).map(_ - 1)
+      val gameID = req.cookies.find(_.name == "gameID")
+      val gameIDValue = gameID.flatMap(id => Option(id.content))
 
-      (numberToGuessValue, attemptsLeftValue) match {
-        case (Some(numberToGuess), Some(remainingAttempts)) if remainingAttempts >= 0 =>
-          if (numberToGuess == guess) {
-            Ok(s"Congratulations $guess was correct!")
-          } else if (remainingAttempts > 0) {
-            if (guess > numberToGuess) {
-              Ok(s"Your guess was too high! $remainingAttempts attempts remain!")
-                .map(_
-                  .addCookie("numberToGuess", numberToGuess.toString)
-                  .addCookie("attemptsLeft", remainingAttempts.toString))
-            } else {
-              Ok(s"Your guess was too low! $remainingAttempts attempts remain!")
-                .map(_
-                  .addCookie("numberToGuess", numberToGuess.toString)
-                  .addCookie("attemptsLeft", remainingAttempts.toString))
-            }
-          } else {
-            if (guess > numberToGuess) {
-              Ok(s"Your guess was too high! You lose!")
+      Ok(s"$gameIDValue")
 
-            } else {
-              Ok(s"Your guess was too low! You lose!")
-            }
-          }
-        case (_, _)                                                                   => Ok("Spoiled cookies... eww!")
-      }
+//      gameIDValue match {
+//        case Some(id) => currentGames.games.find(_.id == id) match {
+//            case Some(game) =>
+//              if (guess == game.numberToGuess) {
+//                currentGames = currentGames.removeGame(game)
+//                Ok(s"Congratulations $guess was correct!").map(_.removeCookie("gameID"))
+//              } else if (game.attempts == 1) {
+//                if (guess > game.numberToGuess) {
+//                  currentGames = currentGames.removeGame(game)
+//                  Ok("Your guess was too high, you lose!").map(_.removeCookie("gameID"))
+//                } else {
+//                  currentGames = currentGames.removeGame(game)
+//                  Ok("Your guess was too low, you lose!").map(_.removeCookie("gameID"))
+//                }
+//              } else {
+//                if (guess > game.numberToGuess) {
+//                  currentGames = currentGames.updateGame(game)
+//                  Ok(s"Your guess was too high! Attempts left: ${game.attempts - 1}")
+//                } else {
+//                  currentGames = currentGames.updateGame(game)
+//                  Ok(s"Your guess was too low! Attempts left: ${game.attempts - 1}")
+//                }
+//              }
+//            case None       => BadRequest("Don't cheat the system!")
+//          }
+//        case None     => BadRequest("You are an alien!")
+//      }
   }.orNotFound
 }
 
@@ -103,16 +125,12 @@ object GuessClient extends IOApp {
     BlazeClientBuilder[IO](ExecutionContext.global).resource.use(client =>
       for {
         _ <- printLine(string = "Providing game parameters:")
-        _ <- client.expect[String](POST(Game(0, 5, 3), uri / "game"))
+        _ <- client.expect[String](POST(Template(0, 5, 3), uri / "game"))
           .flatMap(printLine)
         _ <- printLine()
 
         _ <- printLine(string = "Trying to guess the number:")
-        _ <- client.expect[String](GET(uri / "game" / "guess" / "4")
-          .map(_
-            .addCookie("numberToGuess", "3")
-            .addCookie("attemptsLeft", "3")))
-          .flatMap(printLine)
+        _ <- client.expect[String](GET(uri / "game" / "guess" / "4")).flatMap(printLine)
       } yield ()
     ).as(ExitCode.Success)
 }
